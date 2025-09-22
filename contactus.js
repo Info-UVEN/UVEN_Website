41ebc78d7be79e4d671a9b8d97193caeef9482dc
HithaHarish <hitha22harish@gmail.com>
2025-09-23 01:12:37 +0530

    overwrite main with local code

--- contactus.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("whatsappBtn").addEventListener("click", function () {
-    const phoneNumber = "919036056057";
-    const message = encodeURIComponent("Hello, I'm interested in your services. Please get in touch with me.");
-    const url = `https://wa.me/${phoneNumber}?text=${message}`;
-    window.open(url, "_blank");
-  });
-  
